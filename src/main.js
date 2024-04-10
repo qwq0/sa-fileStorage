1469d8de3b3f0d267dd86a193f1a225f7f6bc439
jingming295 <jingming295@gmail.com>
2024-04-10 19:01:18 +0800

    u

--- src/main.js
+++ src/main.js
@@ -1,12 +1,29 @@
 // app.js
 import express from 'express';
-import upload from './Routes/Upload/index.js';
+import upload from './routes/upload/index.js';
+import bodyParser from 'body-parser';
+import path from 'path';
 const app = express();
 const port = 3000;
 
 const routes = [
     upload
 ]
+const currentWorkingDir = process.cwd();
+app.use('/Image', express.static(path.join(currentWorkingDir, 'Image')));
+
+app.get('/Image/:folder/:filename.:ext', (req, res) => {
+  const { folder, filename, ext } = req.params;
+  const filePath = path.join(currentWorkingDir, 'Image', folder, `${filename}`);
+  res.sendFile(filePath, (err) => {
+    if (err) {
+      console.error('发送文件时出错:', err); // 添加错误日志
+      res.status(404).send('文件未找到');
+    }
+  });
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.send('Hello');
